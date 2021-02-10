011092364b1c99a48ba806f9b7a43b29fe68e319
leobugeja <bugeja.leo@gmail.com>
2021-02-10 16:01:46 +0000

    Update StringerPanels.m
    All stringer-panel root code implemented

--- Wing/StringerPanels.m
+++ Wing/StringerPanels.m
@@ -20,6 +20,10 @@ function  [t,sigma] = simple_panel_buckling(M, c, bh, E)
 end
 
 %% 3.2.1 - 3.2.5
+function weight = stringer_panel_weight(n, b, t, As)
+    weight = b*t*(n+1) + n*As;
+end
+
 function  [n, b, t2, ts, h, d] = root_geometry(M, c, bh, bh_min, E, q) %(n, t2, ts, h, M, c, b2, E)
     % M - root bending moment (Nm)
     % c - root box width (m)
@@ -30,34 +34,73 @@ function  [n, b, t2, ts, h, d] = root_geometry(M, c, bh, bh_min, E, q) %(n, t2, 
 
     N = M/(c*bh); % N - Compressive Load of unit length (N/m)
     
-    weights = {'n', 'b', 't2', 't2', 'ts', 'h', 'd', 'F', 'weight'};
-    min_weight = {'n', 'b', 't2', 't2', 'ts', 'h', 'd', 'F', 'weight'};
+    %% Simple Panel Weight
+    [t_simple, sigma_simple] = simple_panel_buckling(M, c, bh, E); % returns skin thickness and buckling stress for simple panel
+    simple_panel_weight = c*t_simple*1000;
+    
+    %%
+    
+    weights = {'n', 'b', 't', 'ts', 'h', 'd', 'F', 'weight'};
+    min_weight = {'n', 'b', 't', 'ts', 'h', 'd', 'F', 0};
     
     % n - No. of stringers
     % Rt = ts/t = stringer web thickness/skin thickness
-    % Rb = d/b = stringer height/stringer pitch  !!!!! d string height/flange width
-    % d_h = d/h
+    % Rdh = d/h = flange width/flange height
+   
+    % Rb = d*/b = h/b stringer height/stringer pitch  (d* as Gp 17 denote stringer height as d not h)
+    
     for n = 1:16
         for Rt = [0.5,0.6,0.7,0.9,1,1.25,1.5,2]
-            for d_h = [0.3,0.4,0.5]
-                for h_b = 1 %!!!!
-                    % Check h < bh_min/2
-                    As = (h + 2*d)*ts; %As stringer area
-                    % F = FARRAR(Rt, As/(b*t)) IS t=ts or t!!!!!!
-                    % t = solve_skin_thickness(N, d_h, Rt, Rb, h_b, E)
-                    % update min_weight
-                    % weights(end+1,:) = {'n', 'b', 't2', 't2', 'ts', 'h', 'd', 'F', 'weight'}
-        
+            for Rdh = [0.3,0.4,0.5]
+                for Rb = [0.05, 0.1,0.15,0.2,0.25,0.3]
+                    
+                    b = c/(n+1); % b - stringer pitch
+                    h = Rb*b; % h - stringer height
+                    d = Rdh*h; % d - flange height
+                    
+                    if h < bh_min/2 % Check stringer height within min box height
+                        
+                        % Get Optimum FARRAR
+                        As_bt = Rt*(h+2*d)/b; % As/(b*t) = ((h + 2*d)*ts)/(b*t) = Rt*(h+2*d)/b
+                        F = FARRAR(Rt, As_bt) %!!!!!!!!!!!!!
+                        
+                        % Solve for skin thickness using combined loads inequality
+                        t = solve_skin_thickness(N, Rdh, Rt, Rb, E) %!!!!!!!!!!
+                        ts = Rt*t;
+                        
+                        % Calculate Weight Saving
+                        As = As_bt * t * b;
+                        stringer_panel_weight = stringer_panel_weight(n, b, t, As)
+                        weight_save = (simple_panel_weight - stringer_panel_weight)/simple_panel_weight;
+                        
+                        % If new geometry improves weight saving
+                        if min_weight(end) < weight_save
+                            min_weight = {n, b, t, ts, h, d, F, weight_save};
+                        end
+                        % weights(end+1,:) = {'n', 'b', 't2', 't2', 'ts', 'h', 'd', 'F', 'weight'}
+                    else
+                        % print out some message that h is too large?
+                    end
                 end
             end
         end
     end
-                    
-        
-
-
-
     
+    % Purpose of storing all the variables in a cell array is the option to store all different iterations in one array
+    n = min_weight(1)
+    b = min_weight(2)
+    t = min_weight(3)
+    ts = min_weight(4)
+    h = min_weight(5)
+    d = min_weight(6)
+    F = min_weight(7)
+        
+    % Obtain optimum L using Farrer's Equation
+    Kc = get_Kc(h/b, ts/t);
+    sigma_cr = Kc*E*(t/b)^2;
+    L = F^2*N*E/sigma_cr; % From rearanging FARRAR's equation
+
+    %{
     % t2 - Skin Thickness (mm)
     % ts - Stringer web thickness (mm)
     % h - Depth of stringer (mm)
@@ -94,7 +137,7 @@ function  [n, b, t2, ts, h, d] = root_geometry(M, c, bh, bh_min, E, q) %(n, t2, 
     % Equate critical initial buckling stress with critical flexural
     % buckling and rearange for L:
     L = F^2*N*E/(sigma_cr^2*10^12); % L - Rib Spacing
-    
+    %}
 end
 
 %% 3.2.6
